baca395c0ba1b2e3eda54fc54d8372ac9ccabc55
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-07 14:06:14 +0100

    Funktioner för skapande av div
    Här är mina två funktioner som hjälper mig att skapa alla divarna som sedan läggs till i numberContainerDom. Ska testa att minska ner det till en funktion.

--- common.js
+++ common.js
@@ -2,3 +2,20 @@ function randomNumber(min, max) { //genererar ett slumpmässigt nummer
     return min + Math.floor((max + 1 - min) * Math.random());
 };
 
+function numberDivs() { //Ändrad på den 23:e december
+    numberContainerDom.innerHTML = "";
+    let inputValue = createInputDom.value; //eftersom värdet uppdateras 
+    let numberOfDivs = Number(inputValue); // Här omvandlar vi strängen av nummer till nummer.
+
+    for (let i = 0; i < numberOfDivs; i++) {
+        createNumberDivs();
+    }
+};
+
+function createNumberDivs() {
+    let numberDivDom = document.createElement("div");
+    numberDivDom.textContent = randomNumber(0, 99);
+    numberDivDom.classList.add("numberDivsCSS");
+    numberDivDom.classList.add("box");
+    numberContainerDom.appendChild(numberDivDom);
+}
