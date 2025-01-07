14b605302bbbe4999aaac8ff056665bab14918e0
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-07 14:08:32 +0100

    Updatering av funktion numberDIvs
    Jag har uppdaterat och ändrat om så att istället för två funktioner så har vi en funktion som hjälper mig att skapa divarna.

--- common.js
+++ common.js
@@ -8,14 +8,11 @@ function numberDivs() { //Ändrad på den 23:e december
     let numberOfDivs = Number(inputValue); // Här omvandlar vi strängen av nummer till nummer.
 
     for (let i = 0; i < numberOfDivs; i++) {
-        createNumberDivs();
+        let numberDivDom = document.createElement("div");
+        numberDivDom.textContent = randomNumber(0, 99);
+        numberDivDom.classList.add("numberDivsCSS");
+        numberDivDom.classList.add("box");
+        numberContainerDom.appendChild(numberDivDom);
     }
 };
 
-function createNumberDivs() {
-    let numberDivDom = document.createElement("div");
-    numberDivDom.textContent = randomNumber(0, 99);
-    numberDivDom.classList.add("numberDivsCSS");
-    numberDivDom.classList.add("box");
-    numberContainerDom.appendChild(numberDivDom);
-}
