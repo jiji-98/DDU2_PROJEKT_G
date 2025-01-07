921273691f63544f595669c04ef5dd9771ec17f7
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-07 12:48:41 +0100

    ADD_UP_TO html referenser
    Här skapar jag referenser till html element som finns i html filen som kommer vara relevanta i funktionerna.

---
+++ addUpTo/index.js
@@ -0,0 +1,5 @@
+let addUpToContainer = document.querySelector("#addUpToDiv");
+
+let addUpTo_inputDom = document.querySelector("#addUpTo_input");
+
+let addUpTo_buttonDom = document.querySelector("#addUpTo_button");
