dbbee7571413e849a6d1e8bb00f06d52efa91dd8
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-08 14:28:30 +0100

    funktion findRandomNumberInDivs
    Precis som funktionen heter så ska den hitta den hitta divar vars textContent är det samma som ett slumpmässigt nummer.
    
    Det första jag gör är att hämta alla divar genom querySelectorAll, och sedan skapar jag en variabeln som kommer att innehålla ett retur värde från funktionen randomNumber som returnerar ett slumpmässigt nummer.
    
    Jag använder forEach metoden och kollar först: om diven har klassen found_removed så ska vi gå ur funktionen. Det andra vi kollar är om diven innehåller klasen found_remove så tar iv bort den klassen.
    
    Sedan så tilldelar jag värdet av randomNumberInputDom variabel randomNumberForDiv.
    
    Sist så kör jag en if sats för att se om någon av divarnas textContent(omvandlat till ett nummer) som vi går igenom matchar det slumpmässiga nummret i randdomNumberForDiv.
    Om det gör det så lägger vi till klassen found_remove till den.
    
    Och allt dett akommer att hända när jag trycker på knappen New Random Number.

--- remove/index.js
+++ remove/index.js
@@ -1,3 +1,26 @@
+function findRandomNumberInDivs() {
+    let divArray = document.querySelectorAll(".numberDivsCSS");
+    let randomNumberForDiv = randomNumber(0, 99);
+    removedNumberInputDom.value = ` - `;
+
+    divArray.forEach(div => {
+
+        if (div.classList.contains("found_removed")) return;
+
+        if (div.classList.contains("found_remove")) {
+            div.classList.remove("found_remove");
+        }
+        randomNumberInputDom.value = `${randomNumberForDiv}`;
+
+        if (randomNumberForDiv === Number(div.textContent)) {
+            div.classList.add("found_remove");
+
+        }
+    });
+
+}
+
+
 
 let randomNumberButton = document.querySelector("#random_number_button");
 let randomNumberInputDom = document.querySelector("#random_number_box");
@@ -6,3 +29,4 @@ let removeButton = document.querySelector("#remove_button");
 let removedNumberInputDom = document.querySelector("#removed_number_box");
 
 
+randomNumberButton.addEventListener("click", findRandomNumberInDivs);
