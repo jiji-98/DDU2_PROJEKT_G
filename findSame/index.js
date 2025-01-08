2ba336d390ca7cac0485c9c1c6e467ca710630f5
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-08 13:15:29 +0100

    funktion findCopiesOfDiv
    Jag börjar på rad 3 med att dynamiskt hämta alla divarna med klassen numberDivsCSS.
    
    Sedan anvävänder jag foreach metoden och sen lägger jag till en addEventListener på varje div för att jag vill att kodraderna som kommer härnäst ska gälla när jag klickar på vilken av divarna.
    
    på rad 9 så kör jag ytterligare en forEach metod för att jag vill bara hitta kopior av diven jag trycker på ska visas och när jag trycker på en annan div så ska den bli grön och diven innan ska inte bli grön. Så det säkerställer att när jag trycker på en ny div så markeras inte den tidigare diven längre.
    
    sedan på rad 13 så kör jag en for of loop, då jag vill se om det finns andra divar som har samma textContent som diven jag har markerat. Finns det de så får otherDiv och div klassen found och otherDiv läggs till i arrayen tempDivArray.
    
    I else satsen så kommer det stå att det bara finns en div med den div textContent då det är bara diven som trycker på som finns.
    
    När for loopen är klar så använder jag längden på temDivArray för att räkna ut hur många kopior av av den tryckta diven som finns.

---
+++ findSame/index.js
@@ -0,0 +1,30 @@
+function findCopiesOfDiv() {
+
+    let divArray = document.querySelectorAll(".numberDivsCSS");
+
+    divArray.forEach(div => {
+        div.addEventListener("click", function () {
+            let tempDivArray = [];
+
+            divArray.forEach(div => div.classList.remove("found"));
+
+            copiesPDom.textContent = `Click on a number to find copies`;
+
+            for (let otherDiv of divArray) {
+
+                if (div.textContent == otherDiv.textContent) {
+                    div.classList.add("found");
+                    otherDiv.classList.add("found");
+                    tempDivArray.push(otherDiv);
+
+                } else {
+                    copiesPDom.textContent = `1 copies of ${div.textContent}`;
+                };
+            };
+            let numberOfFoundDivs = tempDivArray.length;
+            copiesPDom.textContent = `${numberOfFoundDivs} copies of ${div.textContent}`;
+
+        });
+
+    });
+};
