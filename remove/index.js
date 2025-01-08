0075fd231fe615cb6448459ee43e890869bb96b4
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-08 14:05:44 +0100

    HTML referenser

---
+++ remove/index.js
@@ -0,0 +1,8 @@
+
+let randomNumberButton = document.querySelector("#random_number_button");
+let randomNumberInputDom = document.querySelector("#random_number_box");
+
+let removeButton = document.querySelector("#remove_button");
+let removedNumberInputDom = document.querySelector("#removed_number_box");
+
+
