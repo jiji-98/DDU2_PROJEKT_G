82504b8af3c99968d7432f515dd333267f982d58
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-08 12:21:50 +0100

    function updateInteractivity
    På rad 3 i funktionen så hämtar jag alla divarna som är i div containern numberDivContainerDom genom css klassen numberDIvsCSS. Jag gör detta för att när jag trycker på create knappen så uppdateras divarna i containern, och där med att hämta divarna innanför funktionen så hämtar jagdivarna dynamiskt.
    
    Sedan på rad 5 så använder jag forEach metoden på min "array". Sedan så lägger jag till ett addEventListener "click" eftersom om jag klickar på vilken div som helst så vill jag att något ska hända.
    
    Efter det så kör jag en if sats, när jag trycker på diven, för att se om divarna innehåller klassen div_box, vilket är en klass som inte har några css regler. Och om diven har det, vilket alla divar har från det att de skapas, så tar vi bort den klassen och lägger till en annan klass target som har lite css regler som gör att bakgrundsfärgen ändras när den trycks på och när man han musen över.
    Sen i else så kollar jag när jag trycker på diven om den har klassen target så tar vi bort den och lägger tillbaka klassen div_box, och så lägger jag till ett nytt nummer i divens textContent, genom att anropa funktionen för ett slumpmässigt nummer, då nummret inte ska vara densamma som den var tidigare.

--- clear/index.js
+++ clear/index.js
@@ -1,1 +1,21 @@
+function updateInteractivity() {
+
+    let divArray = document.querySelectorAll(".numberDivsCSS");
+
+    divArray.forEach((div) => {
+        div.addEventListener("click", () => {
+            if (div.classList.contains("div_box")) {
+                div.classList.add("target");
+                div.classList.remove("div_box");
+            } else if (div.classList.contains("target")) {
+                div.classList.remove("target");
+                div.classList.add("div_box");
+                div.textContent = randomNumber(0, 99);
+            }
+        });
+
+    });
+
+};
+
 let clearButton = document.querySelector("#clear_button");
