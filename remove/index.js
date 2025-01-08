9590502d216f2f2ac5f1fd127a5770931a5e367d
jiji-98 <rebecca.jarljohansson@gmail.com>
2025-01-08 14:36:47 +0100

    function removeMarkedDiv
    I nästa del av vad denna sida ska ådtsakomma är att röd markera de tidigare markerade divarna och i removedNumberInputDom så ska vi skriva hur många gånger (hur många divar) som har tagits bort.
    
    Jag börjar i funbktionen med att hämta alla divar som har fått klassen found_remove, från den tidigare funktionen.
    
    Sedan så kontrollerar jag om det inte finns några divar som har klassen found_remove så kommer det stå i p elementet "There is nothing to remove" och vi kommer gå ur funktionen.
    
    Nästa steg är att jag går igenom alla divar som har klasssen found_remove, jag tar bort klassen och lägger till klassen found_removed istället, och ändrar divens textContent till "X".
    
    När forEach metoden har gått igenom alla dessa divar så kommer vi uppdatera removedNumberInputDoms värde var att skriva hur många gånger (hur många divar) som det slumpmässiga numret har tagits bort.
    
    Och allt detta kommer att ske när vi trycker på remove knappen.

--- remove/index.js
+++ remove/index.js
@@ -21,6 +21,23 @@ function findRandomNumberInDivs() {
 }
 
 
+function removeMarkedDiv() {
+    let divsToRemove = document.querySelectorAll(".found_remove");
+
+    if (divsToRemove.length === 0) {
+        removedNumberInputDom.value = `There is nothing to remove`;
+        return;
+    }
+
+    divsToRemove.forEach(div => {
+        div.classList.add("found_removed");
+        div.classList.remove("found_remove");
+        div.textContent = "X";
+    });
+
+    removedNumberInputDom.value = `${randomNumberInputDom.value} removed ${divsToRemove.length} time(s)`;
+
+}
 
 let randomNumberButton = document.querySelector("#random_number_button");
 let randomNumberInputDom = document.querySelector("#random_number_box");
@@ -30,3 +47,5 @@ let removedNumberInputDom = document.querySelector("#removed_number_box");
 
 
 randomNumberButton.addEventListener("click", findRandomNumberInDivs);
+
+removeButton.addEventListener("click", removeMarkedDiv)
